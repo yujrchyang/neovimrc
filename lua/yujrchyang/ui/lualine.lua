75c9f6cbe1dd5099c489e9848e94d0b3a079ca59
yujrchyang <yujrchyang@gmail.com>
2025-12-24 17:10:35 +0800

    show file path in lualine plugin

--- lua/yujrchyang/ui/lualine.lua
+++ lua/yujrchyang/ui/lualine.lua
@@ -10,7 +10,12 @@ require('lualine').setup({
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch' },
-    lualine_c = { 'filename' },
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      }
+    },
     lualine_x = {
       'encoding',
       'fileformat',
